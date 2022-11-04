603de7b53205fdcff0bfdd23c9f61ef3d1d3b544
Nasir Lawal <nasirlawal001@gmail.com>
2022-11-04 13:27:29 +0100

    change links to bootstrap btn

--- src/components/LinksSection.jsx
+++ src/components/LinksSection.jsx
@@ -1,4 +1,5 @@
 import { Route, Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import React from 'react';
 import slack from '../assets/svg/slack.svg';
 import github from '../assets/svg/github.svg'
@@ -6,54 +7,66 @@ import github from '../assets/svg/github.svg'
 const LinksSection = () => {
   return (
     <div className='lg:tw-mx-8'>
+      <style type='text/css'>
+        {`
+          .btn-cust {
+            background-color: #e5e7eb;
+            color: black;
+          }
+          .btn-cust:hover {
+            background-color: #d1d5db;
+            color: black;
+          }
+        `}
+      </style>
       <div className='tw-mx-4 md:tw-mx-12 lg:tw-w-2/3 lg:tw-mx-auto tw-mt-12'>
-        <a className='tw-font-semibold' id='twitter' href="http://twitter.com/_code_for_fun" target="_blank" rel="noopener noreferrer">
-          <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+        <Button href="http://twitter.com/_code_for_fun" variant='cust' id='twitter' className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+          <div className='tw-font-semibold tw-pt-1'>
             Twitter Link
           </div>
-        </a>
+        </Button>
         <div className='tw-mt-4'>
-          <a type='submit' className='tw-font-semibold' id='btn__zuri' href="https://training.zuri.team/" target="_blank" rel="noopener noreferrer">
-            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+          <Button href="https://training.zuri.team/" variant='cust' id='btn__zuri' className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+            <div className='tw-font-semibold tw-pt-1'>
               Zuri Team
             </div>
-          </a>
+          </Button>
         </div>
         <div className='tw-mt-4'>
-          <a type='submit' className='tw-font-semibold' id='books' href="http://books.zuri.team" target="_blank" rel="noopener noreferrer">
-            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+          <Button href="http://books.zuri.team" variant='cust' id='books' className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+            <div className='tw-font-semibold tw-pt-1'>
               Zuri Books
             </div>
-          </a>
+          </Button>
         </div>
         <div className='tw-mt-4'>
-          <a type='submit' className='tw-font-semibold' id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=Nasir" target="_blank" rel="noopener noreferrer">
-            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+          <Button href="https://books.zuri.team/python-for-beginners?ref_id=nasir001" variant='cust' id='book__python' className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+            <div className='tw-font-semibold tw-pt-1'>
               Python Books
             </div>
-          </a>
+          </Button>
         </div>
         <div className='tw-mt-4'>
-          <a type='submit' className='tw-font-semibold' id='pitch' href="https://background.zuri.team" target="_blank" rel="noopener noreferrer">
-            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+          <Button href="https://background.zuri.team" variant='cust' id='pitch' className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+            <div className='tw-font-semibold tw-pt-1'>
               Background Check for Coders
             </div>
-          </a>
+          </Button>
         </div>
         <div className='tw-mt-4'>
-          <a type='submit' className='tw-font-semibold' id='book__design' href="https://books.zuri.team/design-rules" target="_blank" rel="noopener noreferrer">
-            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+          <Button href="https://books.zuri.team/design-rules" variant='cust' id='book__design' className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+            <div className='tw-font-semibold tw-pt-1'>
               Design Books
             </div>
-          </a>
+          </Button>
         </div>
         <div className='tw-mt-4'>
-          <Link to='/contact' id='book__design'>
-            <a className='tw-font-semibold'>
-              <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+          <Link to='/contact' id='book__contact'>
+            <Button variant='cust' className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+              <div className='tw-font-semibold tw-pt-1'>
                 Contact
               </div>
-            </a>
+            </Button>
           </Link>
         </div>
         <div className='tw-flex tw-justify-center tw-mt-8'>
